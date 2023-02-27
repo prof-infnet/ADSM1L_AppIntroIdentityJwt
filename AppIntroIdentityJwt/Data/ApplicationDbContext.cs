c6df39c0e12f6193dfffbb6db73d12a8446cb249
JonhEdsonRibeirodeCa <jonh.carvalho@prof.infnet.edu.br>
2023-02-27 08:35:49 -0300

    Add project files.

---
+++ AppIntroIdentityJwt/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AppIntroIdentityJwt.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppIntroIdentityJwt.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ AppIntroIdentityJwt/Models/LoginCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace AppIntroIdentityJwt.Models
+{
+    public class LoginCredentials
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
